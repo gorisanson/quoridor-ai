76313f6f70ac88c226408a5c37aca49cdba7b32b
Lee Kyutae <gorisanson@gmail.com>
2019-06-29 15:13:00 +0900

    Show "about" on intro

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.2.7';
+const CACHE_NAME = 'quoridor-ai-cache-v0.2.8';
 const urlsToCache = [
     './',
     './style.css',

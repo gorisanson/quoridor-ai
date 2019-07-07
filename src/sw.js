ad16d26e644a95107a4fa2c198fc0897c51e1fbf
Lee Kyutae <gorisanson@gmail.com>
2019-07-07 16:35:36 +0900

    Update service worker version

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.2.22';
+const CACHE_NAME = 'quoridor-ai-cache-v0.2.23';
 const urlsToCache = [
     './',
     './style.css',

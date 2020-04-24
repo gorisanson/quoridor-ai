1df9962601f15b56c577c48f66d1ff84375f7e69
Lee, Kyutae <gorisanson@gmail.com>
2020-04-24 15:45:28 +0900

    Reflect from branch master

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.3.10';
+const CACHE_NAME = 'quoridor-ai-cache-v0.3.11';
 const urlsToCache = [
     './',
     './style.css',

11b93c70baca825d436ad746d6da446275512817
Lee, Kyutae <gorisanson@gmail.com>
2020-06-25 13:21:11 +0900

    Update service worker version ...
    for updating gtag

--- src/sw.js
+++ src/sw.js
@@ -9,7 +9,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.3.11';
+const CACHE_NAME = 'quoridor-ai-cache-v0.3.12';
 const urlsToCache = [
     './',
     './style.css',

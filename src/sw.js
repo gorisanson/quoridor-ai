9859d612d4d7ccae651087db88791f7dd7ad9057
Lee, Kyutae <gorisanson@gmail.com>
2021-09-20 02:03:27 +0900

    Bump service worker version

--- src/sw.js
+++ src/sw.js
@@ -9,7 +9,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.3.13';
+const CACHE_NAME = 'quoridor-ai-cache-v0.3.14';
 const urlsToCache = [
     './',
     './style.css',

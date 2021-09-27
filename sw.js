5c5be9eb45a97b6f7dafdb75adc48b0c15d5ac36
Lee, Kyutae <gorisanson@gmail.com>
2021-09-27 10:38:20 +0900

    Copy from main

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 * https://developers.google.com/web/fundamentals/primers/service-workers/
 */
 
-const CACHE_NAME = 'quoridor-ai-cache-v0.3.15';
+const CACHE_NAME = 'quoridor-ai-cache-v0.3.16';
 const urlsToCache = [
     './',
     './style.css',
